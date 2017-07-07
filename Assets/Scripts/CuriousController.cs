fffbedc8312bdeaddca5c3b5a1a3fe12a1837780
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-07-07 10:38:05 -0400

    move mechanism

---
+++ Assets/Scripts/CuriousController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuriousController : MonoBehaviour {
+
+	private Animator characterAnimController;
+	private bool isJumping;
+
+//	public float moveSpeed;
+	public float jumpForce;
+
+	void Awake () {
+		characterAnimController = GetComponent<Animator> ();
+	}
+	
+	void FixedUpdate () {
+		MoveCurious ();
+	}
+
+	void MoveCurious () {
+		//- Move left and right
+		if (Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.A))
+		{
+			characterAnimController.SetBool ("isWalking", true);
+			transform.Translate (Vector3.left * Time.deltaTime);
+		} 
+		else if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D))
+		{
+			characterAnimController.SetBool ("isWalking", true);
+			transform.Translate (Vector3.right * Time.deltaTime);
+		}
+
+
+		//- Jump
+		if (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W)) 
+		{
+
+			isJumping = true;
+			characterAnimController.SetTrigger ("isJumping");
+
+			transform.Translate (Vector3.up * Time.deltaTime * jumpForce);
+		}
+		else {
+			isJumping = false;
+		}
+	}
+}
