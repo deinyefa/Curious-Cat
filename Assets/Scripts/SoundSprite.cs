bd92ba2e0f5664fc92ea324802033505869416be
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-07-19 12:26:51 -0400

    mute and sound sprite animation

---
+++ Assets/Scripts/SoundSprite.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSprite : MonoBehaviour {
+
+	public Sprite soundSprite;
+	public Sprite muteSprite;
+
+	private Image currentSprite;
+
+	void Start () {
+		currentSprite = GetComponent<Image>();
+	}
+
+	public void ToggleSprite () {
+		if (currentSprite.sprite == soundSprite)
+			currentSprite.sprite = muteSprite;
+		else {
+			currentSprite.sprite = soundSprite;
+		}
+	}
+}
